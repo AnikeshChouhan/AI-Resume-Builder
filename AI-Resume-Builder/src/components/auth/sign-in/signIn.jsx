d794fab72b0ff9311526ce352ee82b23c889943b
Anikesh Chouhan <anikeshmaheshwari558@gmail.com>
2024-08-29 01:22:30 +0530

    Router-initial-face

---
+++ AI-Resume-Builder/src/components/auth/sign-in/signIn.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const signin = () => {
+  return (
+    <div>
+      <h2>Hello You are Signed in now </h2>
+    </div>
+  );
+};
+export default signin;
--- AI-Resume-Builder/src/main.jsx
+++ AI-Resume-Builder/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./components/auth/sign-in/signIn.jsx";
+import Home from "./components/home/Home.jsx";
+import Dashboard from "./components/dashboard/Dashboard.jsx";
 
-createRoot(document.getElementById('root')).render(
+const myRouter = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/auth/sign-in",
+    element: <SignInPage />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={myRouter} />
+  </StrictMode>
+);
